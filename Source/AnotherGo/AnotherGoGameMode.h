023e1f9691eb1239851671810d3f56c1afa287b4
dunteyr <39315568+dunteyr@users.noreply.github.com>
2023-06-12 13:46:30 -0600

    Initial Initial Commit
    Moved the UE5 project into this repository

---
+++ Source/AnotherGo/AnotherGoGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AnotherGoGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AAnotherGoGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AAnotherGoGameMode();
+};
+
+
+
